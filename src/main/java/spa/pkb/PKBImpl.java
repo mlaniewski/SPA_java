88e28edc83150d87262dc59478763fec37418733
Mateusz Łaniewski <mateuszlaniewski98@gmail.com>
2021-05-24 21:04:57 +0200

    poprawki follows

--- src/main/java/spa/pkb/PKBImpl.java
+++ src/main/java/spa/pkb/PKBImpl.java
@@ -111,22 +111,21 @@ public class PKBImpl implements PKB {
         List<Node<ASTNode>> following = new ArrayList<>();
         List<Node<ASTNode>> s1Childs = s1.getParent().getChildren();
         Iterator<Node<ASTNode>> sIt = s1Childs.iterator();
-        Node<ASTNode> end = s1Childs.get(s1Childs.size() - 1);
 
         Node<ASTNode> s = sIt.next();
+        while (!s.getData().equals(s1.getData())) {
+            s = sIt.next();
+        }
+        if (sIt.hasNext()) {
+            s = sIt.next();
+        }
         if (_transient) {
-            while (!s.getData().equals(end.getData())) {
+            while (sIt.hasNext()) {
                 following.add(s);
-                if (!sIt.hasNext()) {
-                    break;
-                }
                 s = sIt.next();
             }
         } else {
-            s = sIt.next();
-            if (!s.getData().equals(end.getData())) {
-                following.add(s);
-            }
+            following.add(s);
         }
         return following;
     }
